34044d1a95dbc909245d838f4ecdccf97189ed0d
Bruno Vilar <brunovilar@gmail.com>
2021-05-22 15:35:26 -0300

    Track additional model runs for intents

--- src/settings.py
+++ src/settings.py
@@ -11,4 +11,8 @@ WORDS_EMBEDDINGS_MODEL_SOURCE = 'https://dl.fbaipublicfiles.com/fasttext/vectors
 MODELS_PATH = Path(SRC_PATH).parent.joinpath('models')
 TRACKING_URI = os.path.join('notebooks', 'mlruns')
 LOGS_ARTIFACTS_PATH = os.path.join(os.pardir, 'data', 'log')
-CATEGORY_CLASSIFICATION_RUN_ID = '2f2ef8c238084aa0b850353d2fa98e57'  # Identifies the 'production' model
+
+# Identifies the 'production' models
+CATEGORY_CLASSIFICATION_RUN_ID = '2f2ef8c238084aa0b850353d2fa98e57'
+USUPERVISED_INTENT_CLASSIFICATION_RUN_ID = '53621ff6f0c64700a07db139202ba635'
+SUPERVISED_INTENT_CLASSIFICATION_RUN_ID = None
