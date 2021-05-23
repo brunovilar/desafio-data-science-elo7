ce14e266b773650da2b0850b32b36a6b13e8157d
Bruno Vilar <brunovilar@gmail.com>
2021-05-23 12:54:32 -0300

    Productize query intent classification

--- src/settings.py
+++ src/settings.py
@@ -9,10 +9,10 @@ WORDS_EMBEDDINGS_MODEL_SOURCE = 'https://dl.fbaipublicfiles.com/fasttext/vectors
 
 # Model Tracking
 MODELS_PATH = Path(SRC_PATH).parent.joinpath('models')
-TRACKING_URI = os.path.join('notebooks', 'mlruns')
+TRACKING_URI = str(Path(SRC_PATH).parent.joinpath('notebooks', 'mlruns'))
 LOGS_ARTIFACTS_PATH = os.path.join(os.pardir, 'data', 'log')
 
 # Identifies the 'production' models
 CATEGORY_CLASSIFICATION_RUN_ID = '2f2ef8c238084aa0b850353d2fa98e57'
 USUPERVISED_INTENT_CLASSIFICATION_RUN_ID = '53621ff6f0c64700a07db139202ba635'
-SUPERVISED_INTENT_CLASSIFICATION_RUN_ID = None
+SUPERVISED_INTENT_CLASSIFICATION_RUN_ID = '3ff29c0d3a524d9c95612cb0d6de54ab'
