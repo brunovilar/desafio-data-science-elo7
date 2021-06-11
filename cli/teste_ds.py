1484fec53411a52758179f027b8d21881af2cfe0
Bruno Vilar <brunovilar@gmail.com>
2021-06-11 13:34:27 -0300

    Refine model usage log

--- cli/teste_ds.py
+++ cli/teste_ds.py
@@ -16,32 +16,40 @@ logger = LoggerFactory.get_logger(__name__)
 
 
 @validate_arguments()
-def classify_product(product: Annotated[Json, {"format": "json-string"}]) -> None:
-    logger.info(f'Classify Product', extra={'props': {'category':'action_call',
-                                                      'input': product}})
+def classify_product(product: Annotated[Json, str]) -> None:
+
+    logger.info('Classify Product', extra={'props': {
+                'action': 'classify_product_category',
+                'log_type': 'application_usage',
+                'input': product,
+                'input_length': 1}})
+
     product_obj = Product(**product)
     product_obj = fp.first(make_batch_predictions([product_obj]))
     print(product_obj.category)
-    logger.info(f'Classified Product', extra={'props': {'category':'action_result',
-                                                        'input_product': product,
-                                                        'output_product': product_obj.to_dict()}})
 
 
 @validate_arguments()
 def classify_query(query: str) -> None:
-    logger.info(f'Classify Query', extra={'props': {'category':'action_call',
-                                                    'input': query}})
+
+    logger.info('Classify Query', extra={'props': {
+                'action': 'classify_query_intent',
+                'log_type': 'application_usage',
+                'input': query,
+                'input_length': 1}})
+
     query_intent = fp.first(make_supervised_intent_classification([query]))
     print(query_intent)
-    logger.info(f'Classified Query', extra={'props': {'category':'action_result',
-                                                      'input': query,
-                                                      'query_intent': query_intent}})
 
 
 @validate_arguments()
 def recommend(query: str) -> None:
-    logger.info(f'Recommend for Query', extra={'props': {'category':'action_call',
-                                                         'input': query}})
+    logger.info('Recommend for Query', extra={'props': {
+                'action': 'recommend_products_for_query',
+                'log_type': 'application_usage',
+                'input': query,
+                'input_length': 1}})
+
     for product in make_recommendations_for_query(query):
         print(f' - Product ID: {product.product_id} | Title: {product.title}')
 
@@ -77,7 +85,7 @@ def main():
                 for error in e.errors()
             ])
             logger.exception('Input Validation Exception')
-            print(f'Problemas encontrados nos dados de entrada: {validation_issues}')
+            print(f'Problems found on input data: {validation_issues}')
         except:
             logger.exception('Unexpected Exception')
     else:
