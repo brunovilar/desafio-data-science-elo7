acc2976a664618e5d53b8c56517aa35d795bc383
Bruno Vilar <brunovilar@gmail.com>
2021-06-13 09:12:30 -0300

    Add test for comput stats for numeric values function

--- tests/pipeline/test_training_pipeline.py
+++ tests/pipeline/test_training_pipeline.py
@@ -1,9 +1,12 @@
 import pytest
 import numpy as np
 import pandas as pd
+from itertools import product
 from typing import List
+from collections.abc import Iterable
 from src.pipeline.training_pipeline import (compute_embeddings_frame,
-                                            compute_average_embeddings)
+                                            compute_average_embeddings,
+                                            compute_stats_for_numeric_values)
 
 
 all_text_columns = ['column_empty', 'column_letter',
@@ -30,13 +33,14 @@ def text_frame() -> pd.DataFrame:
 
 
 @pytest.fixture
-def embeddings_frame() -> pd.DataFrame:
+def numeric_frame() -> pd.DataFrame:
     return pd.DataFrame(
         {'direct_zero_average_embedding': [0, 0, 0, 0],
          'indirect_zero_average_embedding': [-50, 100, 50, -100],
          'positive_average_embedding': [100, 50, 150, 140],
          'negative_average_embedding': [-100, 50, 150, -140],
          'same_positive_embedding': [15, 15, 15, 15],
+         'compound_embedding': [[5, -15], [15, -5], [8, -3], [12, -17]],
          'category': ['cat_a', 'cat_a', 'cat_a', 'cat_a']
          }
     )
@@ -76,19 +80,21 @@ def test_compute_embeddings_frame(text_frame: pd.DataFrame, columns_to_encode: L
     assert 'category' not in result_columns and f'category_embedding' not in result_columns
 
 
-@pytest.mark.parametrize("columns", [all_numeric_columns, no_column])
+@pytest.mark.parametrize("columns", 
+                         [all_numeric_columns + ['compound'], no_column])
 @pytest.mark.parametrize("categories", [['cat_a'], []])
-def test_compute_average_embeddings(embeddings_frame, columns, categories):
+def test_compute_average_embeddings(numeric_frame, columns, categories):
 
     expected_answers = {'direct_zero_average': 0,
                         'indirect_zero_average': 0,
                         'positive_average': 110,
                         'negative_average': -10,
-                        'same_positive': 15
+                        'same_positive': 15,
+                        'compound': [10, -10]
                         }
 
     average_dict = compute_average_embeddings(
-        embeddings_frame, columns, categories)
+        numeric_frame, columns, categories)
 
     assert len(average_dict) == (len(columns) * len(categories)), \
         "Unexpected number of result elements"
@@ -96,4 +102,37 @@ def test_compute_average_embeddings(embeddings_frame, columns, categories):
     for (category, column), value in average_dict.items():
         assert category in categories, 'Category used was not expected'
         assert column in expected_answers, 'Column used is was not expected'
-        assert value == expected_answers.get(column), 'Wrong value computed'
+
+        if isinstance(value, Iterable):
+            assert all(value == expected_answers.get(column)), \
+            'Wrong value computed'
+        else:
+            assert value == expected_answers.get(column), 'Wrong value computed'
+
+
+@pytest.mark.parametrize("columns", [all_numeric_columns])
+@pytest.mark.parametrize("stats", [['mean'], ['mean', 'min']])
+def test_compute_stats_for_numeric_values(numeric_frame, columns, stats):
+
+    expected_answers = {'direct_zero_average': {'mean': 0, 'min': 0},
+                        'indirect_zero_average': {'mean': 0, 'min': -100},
+                        'positive_average': {'mean': 110, 'min': 50},
+                        'negative_average': {'mean': -10, 'min': -140},
+                        'same_positive': {'mean': 15, 'min': 15}
+                        }
+
+    columns_to_rename = {c: c.replace('_embedding', '')
+                         for c in numeric_frame.columns}
+    numeric_frame = numeric_frame.rename(columns=columns_to_rename)
+
+    stats_dict = compute_stats_for_numeric_values(numeric_frame, columns)
+
+    assert len(stats_dict) == len(columns), \
+        "Unexpected number of result elements"
+
+    for (column, internal_dict) in stats_dict.items():
+        assert column in expected_answers, 'Column used is was not expected'
+        column_stats = expected_answers.get(column)
+        for expected_stat, expected_value in column_stats.items():
+            assert expected_value == internal_dict.get(expected_stat),\
+                 'Wrong value computed'
